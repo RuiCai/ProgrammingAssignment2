eaa68acb476f68aa4e405000027c04072af2b113
RuiCai <ruicaismile@outlook.com>
2014-12-21 17:37:45 +0800

    2nd

--- cachematrix.R
+++ cachematrix.R
@@ -7,12 +7,12 @@ makeCacheMatrix <- function(x = matrix()) {
   m<- NULL
   set<-function(y){
      x<<-y
-     m<<-NULL
+     m<<-NULL               ##set可以x<-y
   }
-  get<-function() x
-  setsolve<-function(solve) m<<-solve
+  get<-function() x 
+  setsolve<-function(solve) m<<-solve     ##setsolve可以修改m<-solve
   getsolve<-function() m
-  list(set=set,get=get,setsolve=setsolve,getsolve=getsolve)
+  list(set=set,get=get,setsolve=setsolve,getsolve=getsolve) ##返回列表，以便调用
 
 }
 
@@ -21,13 +21,13 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
   m<-x$getsolve()
-  if(!is.null(m)){
+  if(!is.null(m)){                     ##判断是否已经有缓存值
     message("getting cached data")
     return(m)
   }
-  data<-x$get()
-  d<-as.matrix(data)
-  m<-solve(d)
+  
+  data<-x$get()                  ##没有则计算逆矩阵
+  m<-solve(data)
   x$setsolve(m)
   m
         ## Return a matrix that is the inverse of 'x'
